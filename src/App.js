669ce9cd81db9fb1a01bb01af15d2c7c34c0ca02
WHMHM WICKRAMASINGHE <140105354+HWickramasinghe@users.noreply.github.com>
2025-03-17 20:24:09 +0530

    LoginandRegistration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import LoginRegister from './Components/LoginRegister/LoginRegister';
+
+function App() {
+  return (
+    <div>
+
+      <LoginRegister/>
+    
+    
+    </div>
+  );
+}
+
+export default App;
